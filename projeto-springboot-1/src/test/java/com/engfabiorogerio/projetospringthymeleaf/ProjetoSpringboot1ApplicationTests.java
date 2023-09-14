d0930a25af57e6214bf48eadf33033f6c10ba637
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-14 18:09:37 -0300

    Create databade method connection and ProductModel

---
+++ projeto-springboot-1/src/test/java/com/engfabiorogerio/projetospringthymeleaf/ProjetoSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engfabiorogerio.projetospringthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
