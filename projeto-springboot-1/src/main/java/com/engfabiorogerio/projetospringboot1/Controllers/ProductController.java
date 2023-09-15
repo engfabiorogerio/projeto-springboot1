f78d05f886f66d48d98e099857f2103f874c89eb
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-15 11:58:21 -0300

    Create GET method

--- projeto-springboot-1/src/main/java/com/engfabiorogerio/projetospringboot1/Controllers/ProductController.java
+++ projeto-springboot-1/src/main/java/com/engfabiorogerio/projetospringboot1/Controllers/ProductController.java
@@ -1,12 +1,17 @@
 package com.engfabiorogerio.projetospringboot1.Controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.apache.catalina.mbeans.MBeanUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +37,18 @@ public class ProductController {
 		
 	}
 	
-
+	@GetMapping("/products")
+	public ResponseEntity<List<ProductModel>> getAllProducts(){
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProduct(@PathVariable(value ="id") UUID id){
+		Optional<ProductModel> product0 = productRepository.findById(id);
+		if(product0.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+	}
 }
